ebab52f3c29caf7839bf2b874943fde0a83a4e46
Fabricio Ceppi <fabricioceppi@gmail.com>
2021-09-13 19:56:02 -0300

    main and menu working allright

--- main.js
+++ main.js
@@ -3,32 +3,51 @@
 const body = document.querySelector('body');
 const mainComponents = document.getElementById('main-components');
 const menu = document.getElementById('menu-button');
+const dynamicContent = document.querySelectorAll('.dynamic');
+let contentCursor = 0;
 
 menu.addEventListener('click', () => {
-    let backColor = getComputedStyle(mainComponents).backgroundColor === 'rgb(229, 41, 27)' ? 'rgb(6, 6, 6)' : 'rgb(229, 41, 27)';
-    let screenWidth = window.innerWidth;
-    let screenHeight = window.innerHeight;
-    let diam = Number(getComputedStyle(menu).width.slice(0, 2));
+    let backColor = contentCursor === 1 ? 'rgb(6, 6, 6)' : 'rgb(229, 41, 27)';
+    let rectWidth = Number(getComputedStyle(mainComponents).width.replace('px', ''));
+    let rectHeight = Number(getComputedStyle(mainComponents).height.replace('px', ''));
+    let diam = Number(getComputedStyle(menu).width.replace('px', ''));
     let augmentBy;
 
-    if (screenWidth > screenHeight) {
-        augmentBy = (Math.sqrt(screenWidth**2 *2)) / diam;
+    if (rectWidth > rectHeight) {
+        augmentBy = (Math.sqrt(rectWidth**2 *2)) / diam;
     } else {
-        augmentBy = (Math.sqrt(screenHeight**2 *2)) / diam;
+        augmentBy = (Math.sqrt(rectHeight**2 *2)) / diam;
     };
-
     augmentBy *= 2;
+    menu.style.transition = 'background-color 0.5s, transform 0.8s';
     menu.style.backgroundColor = backColor;
     menu.style.transform = `scale(${augmentBy})`;
+
     setTimeout(() => {
-        menu.style.transition = 'opacity 0.5s';
+        menu.style.transition = 'opacity 0.3s';
         mainComponents.style.backgroundColor = backColor;
-        menu.style.opacity = 0;
+        menu.style.opacity = '0';
         setTimeout(() => {
             menu.style.backgroundColor = '#f1f1f1';
             menu.style.transform = 'scale(1)';
             menu.style.opacity = '1';
-        },500);
-    }, 500);
-    menu.style.transition = 'background-color 0.5s, transform 0.8s';
+        },400);
+    }, 400);
+
+    if (contentCursor === 1) {
+        contentCursor = 0;
+    } else {
+        contentCursor = 1;
+    }
+    updateContent();
 });
+
+function updateContent() {
+    for (let i = 0; i < dynamicContent.length; i++) {
+        if (dynamicContent[i].classList[1] === 'active') {
+            dynamicContent[i].classList.remove('active');
+        }
+    }
+    dynamicContent[contentCursor].classList.add('active');
+}
+
